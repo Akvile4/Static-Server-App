aa96b4e70b741ed41d931209e42184c6c5590ac9
Akvile4 <aqua0818@gmail.com>
2022-02-14 14:32:50 +0200

    added navbar to different pages

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const port = 5001;
 
-    // makes root 
+    // "static" is our route path, "public" is our folder where "static" files is
 app.use("/static", express.static("public"));
+
+app.use("/about", express.static("about"));
+
+app.use("/contacts", express.static("contacts"));
 
     // run the express application with this function
 app.listen(port, () => {
